f1363c8969ddfeb8cee59f2798dc3cf22b394f4a
Anh Pham <anhphamduy@outlook.com>
2019-02-21 10:49:20 +1100

    Updates

---
+++ precache-manifest.78f51f40fed0ead9e25893e535cb2f3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b03f3406f74901d953fc",
+    "url": "/static/css/main.cc576535.chunk.css"
+  },
+  {
+    "revision": "b03f3406f74901d953fc",
+    "url": "/static/js/main.b03f3406.chunk.js"
+  },
+  {
+    "revision": "5a7dc1a74a79eb5b65fe",
+    "url": "/static/css/1.38214512.chunk.css"
+  },
+  {
+    "revision": "5a7dc1a74a79eb5b65fe",
+    "url": "/static/js/1.5a7dc1a7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a5e01254b161b46db78c9976275aaf52",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.99d06ffed88ed64c719ed4941accc316.js"
+  "/precache-manifest.78f51f40fed0ead9e25893e535cb2f3a.js"
 );
 
 workbox.clientsClaim();
